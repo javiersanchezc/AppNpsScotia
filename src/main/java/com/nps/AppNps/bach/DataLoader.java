01c25ef06b820bc5f5b999fa7eca39489c408119
Javier Antonio Sanchez <jscoderdev@gmail.com>
2024-06-19 19:48:37 -0500

    implementacion  de proceso masivo

---
+++ src/main/java/com/nps/AppNps/bach/DataLoader.java
@@ -0,0 +1,39 @@
+package com.nps.AppNps.bach;
+
+import com.nps.AppNps.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader {
+
+    private final IBPulseService service;
+    private final IOptOutService serviceOut;
+    private final INPSService INPSservice;
+    private final IHuddlesService Huddleservice;
+    private final ICPulseService CPulseservice;
+    private final ICardifService Cardifservice;
+    private final IBPulseService BPulseservice;
+
+    @Autowired
+    public DataLoader(IBPulseService service, IOptOutService serviceOut, INPSService INPSservice,
+                      IHuddlesService Huddleservice, ICPulseService CPulseservice,
+                      ICardifService Cardifservice, IBPulseService BPulseservice) {
+        this.service = service;
+        this.serviceOut = serviceOut;
+        this.INPSservice = INPSservice;
+        this.Huddleservice = Huddleservice;
+        this.CPulseservice = CPulseservice;
+        this.Cardifservice = Cardifservice;
+        this.BPulseservice = BPulseservice;
+    }
+
+    public void loadAllData() {
+        service.loadMasive();
+        serviceOut.loadMasive();
+        Huddleservice.loadMasive();
+        CPulseservice.loadMasive();
+        Cardifservice.loadMasive();
+        BPulseservice.loadMasive();
+    }
+}
